9245ea0dec14272b620a0da75e055a7e881a9318
Clifton McCook <cliftonmccook@gmail.com>
2021-11-06 21:13:19 -0500

    Ex 5 - edited world.php, world.js for cities

--- world.js
+++ world.js
@@ -1,7 +1,9 @@
 window.onload = () => {
     document.getElementsByTagName("input")[0] = '';
     let button = document.getElementById("lookup");
+    let city = document.getElementById("lookup-cities");
     button.addEventListener("click", getCountryInfo);
+    city.addEventListener("click", getCityInfo);
 };
 
 function getCountryInfo(event) {
@@ -10,6 +12,25 @@ function getCountryInfo(event) {
     let key = document.getElementsByTagName("input")[0].name;
     let val = document.getElementsByTagName("input")[0].value;
     let url = actionScript+'?'+key+'='+val;
+    fetch(url)
+    .then(response => {
+        if(response.ok) {
+            return response.text();
+        } else {
+            return Promise.reject("Promise rejected");
+        }
+    })
+    .then(data => {document.getElementById("result").innerHTML = data;})
+    .catch(error => alert("Error fetching data: " + error));
+}
+
+
+function getCityInfo() {
+    let context = 'cities';
+    let actionScript = "world.php";
+    let key = document.getElementsByTagName("input")[0].name;
+    let val = document.getElementsByTagName("input")[0].value;
+    let url = actionScript+'?'+key+'='+val+'&'+'context'+'='+context;
     fetch(url)
     .then(response => {
         if(response.ok) {
--- world.php
+++ world.php
@@ -6,28 +6,53 @@ $dbname = 'world';
 
 if ($_SERVER["REQUEST_METHOD"] == "GET") {
   $country = htmlspecialchars(strip_tags($_GET["country"]));
-  try {
-    $conn = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8mb4", $username, $password);
-    $stmt = $conn->query("SELECT * FROM countries WHERE name LIKE '%$country%'");
+  $context = htmlspecialchars(strip_tags($_GET["context"]));
+  if($context == '') {
+    try {
+      $conn = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8mb4", $username, $password);
+      $stmt = $conn->query("SELECT * FROM countries WHERE name LIKE '%$country%'");
+    
+      $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
+    
+      // Display result as list item
+      // echo "<ul>";
+      // foreach ($results as $row):
+      //   echo "<li>{$row['name']} is ruled by {$row['head_of_state']}</li>";
+      // endforeach;
+      // echo "</ul>";
   
-    $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
+      // Result as table
+      echo "<table>";
+      echo "<tr><th>Country Name</th><th>Continent</th><th>Independence Year</th><th>Head of State</th></tr>";
+      foreach ($results as $row):
+        echo "<tr><td>{$row['name']}</td><td>{$row['continent']}</td><td>{$row['independence_year']}</td><td>{$row['head_of_state']}</td></tr>";
+      endforeach;
+      echo "</table>";
+    } catch(PDOException $e) {
+      echo "Error: " . $e->getMessage();
+    }
+  }
   
-    // Display result as list item
-    // echo "<ul>";
-    // foreach ($results as $row):
-    //   echo "<li>{$row['name']} is ruled by {$row['head_of_state']}</li>";
-    // endforeach;
-    // echo "</ul>";
-
-    // Result as table
-    echo "<table>";
-    echo "<tr><th>Country Name</th><th>Continent</th><th>Independence Year</th><th>Head of State</th></tr>";
-    foreach ($results as $row):
-      echo "<tr><td>{$row['name']}</td><td>{$row['continent']}</td><td>{$row['independence_year']}</td><td>{$row['head_of_state']}</td></tr>";
-    endforeach;
-    echo "</table>";
-  } catch(PDOException $e) {
-    echo "Error: " . $e->getMessage();
+  if($context == 'cities') {
+    try{
+      $conn = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8mb4", $username, $password);
+      $stmt = $conn->query("SELECT * FROM countries JOIN cities ON countries.code=cities.country_code WHERE countries.name LIKE '%$country%';");
+  
+      $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
+  
+      // Result as table
+      echo "<table>";
+      echo "<tr><th>Name</th><th>District</th><th>Population</th></tr>";
+      foreach ($results as $row):
+        echo "<tr><td>{$row['name']}</td><td>{$row['district']}</td><td>{$row['population']}</td></tr>";
+      endforeach;
+      echo "</table>";
+  
+      // echo "Result returned successfully";
+  
+    } catch(PDOException $e){
+      echo "Error: " . $e->getMessage();
+    }
   }
 }
 $conn = null;
