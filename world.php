b14c0be261ff112137fa069d18f0489761006548
Clifton McCook <cliftonmccook@gmail.com>
2021-11-06 20:59:06 -0500

    Ex 4 - edited world.php and world.css for table

--- world.php
+++ world.php
@@ -13,11 +13,19 @@ if ($_SERVER["REQUEST_METHOD"] == "GET") {
     $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
   
     // Display result as list item
-    echo "<ul>";
+    // echo "<ul>";
+    // foreach ($results as $row):
+    //   echo "<li>{$row['name']} is ruled by {$row['head_of_state']}</li>";
+    // endforeach;
+    // echo "</ul>";
+
+    // Result as table
+    echo "<table>";
+    echo "<tr><th>Country Name</th><th>Continent</th><th>Independence Year</th><th>Head of State</th></tr>";
     foreach ($results as $row):
-      echo "<li>{$row['name']} is ruled by {$row['head_of_state']}</li>";
+      echo "<tr><td>{$row['name']}</td><td>{$row['continent']}</td><td>{$row['independence_year']}</td><td>{$row['head_of_state']}</td></tr>";
     endforeach;
-    echo "</ul>";
+    echo "</table>";
   } catch(PDOException $e) {
     echo "Error: " . $e->getMessage();
   }
